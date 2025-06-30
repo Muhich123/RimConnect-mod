017437a880b55aed1ede31930e163d2c7c622ec5
Muhich123 <142428323+Muhich123@users.noreply.github.com>
2025-06-30 08:04:24 +0300

    Switch to DonationAlerts integration

--- Source/RimConnection/RimConnection.cs
+++ Source/RimConnection/RimConnection.cs
@@ -1,26 +1,37 @@
-﻿using UnityEngine;
+using UnityEngine;
 using Verse;
 
 namespace RimConnection
 {
-    public class RimConnection: Mod
+    public class RimConnection : Mod
     {
         RimConnectSettings settings;
 
-
         public RimConnection(ModContentPack content) : base(content)
         {
-            this.settings = GetSettings<RimConnectSettings>();
+            settings = GetSettings<RimConnectSettings>();
+            InitializeEventList();
         }
 
         public override void DoSettingsWindowContents(Rect inRect)
         {
-            this.settings.DoWindowContents(inRect);
+            settings.DoWindowContents(inRect);
         }
 
         public override string SettingsCategory()
         {
             return "RimConnect";
+        }
+
+        private void InitializeEventList()
+        {
+            ActionList.GenerateActionLookup();
+            var validCommands = ActionList.ActionListToApi().validCommands;
+            Settings.CommandOptionListController.commandOptionList = new CommandOptionList()
+            {
+                commandOptions = validCommands.ConvertAll(vc => vc.toCommandOption())
+            };
+            Log.Message($"[RimConnect] Initialized {Settings.CommandOptionListController.commandOptionList.commandOptions.Count} events locally.");
         }
     }
 }
